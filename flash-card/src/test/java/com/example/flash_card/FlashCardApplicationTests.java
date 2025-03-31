7bde0bba58c96cb02076aec4d000322b86db20c4
Nghia Trung <nghia250803@gmail.com>
2025-03-31 17:51:46 +0700

    Update tài liệu yêu cầu, tài liệu thiết kế, khởi động Spring Boot với homepage

---
+++ flash-card/src/test/java/com/example/flash_card/FlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flash_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
