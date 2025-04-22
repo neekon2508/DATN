ea7d87a9a5b63526583c4d964e95d0d933cfd06d
Nghia Trung <nghia250803@gmail.com>
2025-04-22 17:12:51 +0700

    Delete

--- flash-card-admin/src/main/java/repository/AccountUserRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import entity.AccountUser;
-
-public interface AccountUserRepository extends CrudRepository<AccountUser, Long>{
-    AccountUser findByUsername(String username);
-}
